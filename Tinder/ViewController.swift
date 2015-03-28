eb9b2f355b2208805498933cfebd437a97b13f34
SterlingSavariau <Sterling.Savariau@gmail.com>
2015-03-28 03:46:51 -0400

    First commit adding Parse and FacebookSDK frameworks.

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Sterling Savariau on 11/5/14.
+//  Copyright (c) 2014 Sterling Savariau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var permissions = ["public_profile"]
+        
+        PFFacebookUtils.logInWithPermissions(permissions, {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user == nil {
+                NSLog("Uh oh. The user cancelled the Facebook login.")
+            } else if user.isNew {
+                NSLog("User signed up and logged in through Facebook!")
+            } else {
+                NSLog("User logged in through Facebook!")
+            }
+        })
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
